eeff81839dbb2596a31f0d9c9dbb00bc2cb7dce1
Nikolai Atlasov <atlasovnik@gmail.com>
2025-02-04 01:49:20 +1000

    Это я пытаюсь понять что такое int, str, float...
    Надеюсь получится понять

---
+++ study2.py
@@ -0,0 +1,4 @@
+#koroche ya ne ochen ponyal no esli stavlu "int" to poluchaetsa celie chisla, a esli "float" to decyatie i sotie....
+my_vozrast = int(input("Vozrast Nikolai:"))
+Vova_vozrast = (my_vozrast - 5)
+print("Vova vozrast is" + str(Vova_vozrast))
